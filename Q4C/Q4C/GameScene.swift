6f6ce5b3fe7edfdd8acf0601f098b6032ecd99fd
Leo <leonid@ac93.org>
2018-01-14 16:09:06 -0500

    Did a lotta stuff
    Waves in own class (still in gameScene.swift tho), figured out SKLabelNodes and SKSpriteNodes, pause button literally just pauses the game and then can resume it lmao

--- Q4C/Q4C/GameScene.swift
+++ Q4C/Q4C/GameScene.swift
@@ -12,85 +12,95 @@ import GameplayKit
 class GameScene: SKScene {
     
     private let universe : Universe = Universe()
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-//    private var firstSpot : CGPoint?
-//    private var secondSpot : CGPoint?
-//    private var line : SKShapeNode?
+    
+    let pauseButtonNode = SKSpriteNode(imageNamed: "pauseButton")
+    
+    //private var label : UILabel = UILabel()
+    
     private var touchGravity : SKFieldNode?
-    private var radiusOfUnaffectedCircle : CGFloat = 50
-    private var pointsOfCircle : [SKNode] = []
+    private var startGravity = SKFieldNode.radialGravityField()
+    
+    private let radiusOfUnaffectedCircle : CGFloat = 50
+    private let numPointsInWave = 180
     private let massOfPoint : CGFloat = 0.5
-    //private var circlePath : CGMutablePath = CGMutablePath.init()
-    private var circle : SKShapeNode = SKShapeNode()
+    
+    private var waves : [Wave] = []
+    private var numWaves : Int = 0
+    private var originPoint : CGPoint = CGPoint()
+    
+    private let startGravityStrength : Float = -0.1
+    
+    private var startTime : TimeInterval = 0
+    private let timeBetweenWaves : TimeInterval = 4.0
+    
+    private let backgroundColorComponent : CGFloat = 48 / 255
+    private let waveColorComponent : CGFloat = 255 / 255
     
     override func didMove(to view: SKView) {
         //initialize the physics circle
-        initCircle()
+        self.backgroundColor = UIColor(red : backgroundColorComponent, green : backgroundColorComponent, blue : backgroundColorComponent, alpha : 1)
+        originPoint = CGPoint(x: -self.size.width / 2, y: -self.size.height / 2)
+        startTime = NSDate().timeIntervalSince1970
+        startGravity.strength = startGravityStrength
+        startGravity.position = originPoint
+        addChild(startGravity)
         
-//        firstSpot = CGPoint(x: -self.size.width / 2, y: -self.size.height / 2)
-//        secondSpot = CGPoint(x: -self.size.width / 2, y: -self.size.height / 2)
-//        line = SKShapeNode()
-//        addChild(line!)
+        let label1 = SKLabelNode(text: "test text node")
+        label1.fontColor = UIColor(red: waveColorComponent, green: waveColorComponent, blue: waveColorComponent, alpha: 1)
+        label1.fontName = "PingFangTC-Ultralight"
+        label1.position = CGPoint(x : 0, y : 0)
+        label1.color = UIColor(red: backgroundColorComponent, green: backgroundColorComponent, blue: backgroundColorComponent, alpha: 1)
+        //addChild(label1)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: (self.view?.bounds.width)!, height: (self.view?.bounds.height)!))
+        label.center = CGPoint(x : (self.view?.bounds.width)! / 2, y : (self.view?.bounds.height)! / 2)
+        label.textAlignment = NSTextAlignment.center
+        label.text = "Test text"
+        label.textColor = UIColor(red: waveColorComponent, green: waveColorComponent, blue: waveColorComponent, alpha: 1)
+        label.font = UIFont(name: "PingFangTC-Ultralight", size: 15)
+        //self.view?.addSubview(label)
+        
+        
+        
+        if self.size.width > self.size.height {
+            pauseButtonNode.size = CGSize(width: self.size.height / 20, height: self.size.height / 20)
+        }
+        else {
+            pauseButtonNode.size = CGSize(width : self.size.width / 20, height : self.size.width / 20)
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        //pauseButtonNode.size = CGSize(width : self.size.width / 16, height : self.size.height / 16)
+        pauseButtonNode.color = UIColor(red: waveColorComponent, green: waveColorComponent, blue: waveColorComponent, alpha: 1)
+        pauseButtonNode.position = CGPoint(x : -(self.size.width / 2) + pauseButtonNode.size.width * 3 / 2, y : (self.size.height / 2) - pauseButtonNode.size.height * 3 / 2)
+        addChild(pauseButtonNode)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        
+        
+        waves.append(Wave(scene: self, originPoint: originPoint, startingRadius: radiusOfUnaffectedCircle, numPointsInWave: numPointsInWave, massOfPoint: massOfPoint, startColorComponent: waveColorComponent, endColorComponent: backgroundColorComponent))
     }
     
-    
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-        
         touchGravity = SKFieldNode.radialGravityField()
         touchGravity?.position = pos
         touchGravity?.strength = -1
         addChild(touchGravity!)
+        
+        if pauseButtonNode.contains(pos) {
+            self.isPaused = !self.isPaused
+        }
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        touchGravity?.position = pos
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        
         touchGravity?.removeFromParent()
         touchGravity = nil
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
@@ -107,33 +117,71 @@ class GameScene: SKScene {
     }
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        //line!.removeFromParent()
-        updateCircle()
-//        secondSpot!.x += 3
-//        firstSpot!.y += 3
-//        let midWaySpot : CGPoint = CGPoint(x: (firstSpot!.x + secondSpot!.x)/2, y: (firstSpot!.y + secondSpot!.y)/2)
-//        let origin = CGPoint(x: -self.size.width / 2, y: -self.size.height / 2)
-//        let path = CGMutablePath.init()
-//        path.move(to: firstSpot!)
-//        //path.addLine(to: secondSpot!)
-//        path.addQuadCurve(to: secondSpot!, control: origin)
-//        line!.path = path
-        //line!.strokeColor = UIColor.white
-        //line!.lineWidth = 2
-        //addChild(line!)
+        
+        let actualCurrentTime = NSDate().timeIntervalSince1970
+        let elapsedTime = actualCurrentTime - startTime
+        
+        if Int(elapsedTime / timeBetweenWaves) > numWaves {
+            waves.append(Wave(scene: self, originPoint: originPoint, startingRadius: radiusOfUnaffectedCircle, numPointsInWave: numPointsInWave, massOfPoint: massOfPoint, startColorComponent : waveColorComponent, endColorComponent : backgroundColorComponent))
+            numWaves+=1
+        }
+        
+        var index : Int = 0
+        for wave in waves {
+            wave.updateCircle()
+            if (wave.toBeDestroyed()) {
+                waves.remove(at: index)
+            }
+            index += 1
+        }
     }
     
-    func initCircle() {
+    static func vectorFromPoints(point1 : CGPoint, point2 : CGPoint) -> CGVector {
+        let dx = point2.x - point1.x
+        let dy = point2.y - point1.y
+        return CGVector(dx: dx, dy: dy)
+    }
+}
+
+class Wave {
+    
+    private let birthTime : TimeInterval
+    private let lifeSpan : TimeInterval = 15
+    private let originPoint : CGPoint
+    private let numPointsInWave : Int
+    private var pointsOfCircle : [SKNode] = []
+    private let massOfPoint : CGFloat
+    private let startingRadius : CGFloat
+    private let circle : SKShapeNode
+    private let scene : SKScene
+    private let startColorComponent : CGFloat
+    private let endColorComponent : CGFloat
+    private var toBeDestroyedVar : Bool = false
+    
+    init(scene : SKScene, originPoint : CGPoint, startingRadius : CGFloat, numPointsInWave : Int, massOfPoint : CGFloat, startColorComponent : CGFloat, endColorComponent : CGFloat) {
+        birthTime = NSDate().timeIntervalSince1970
+        self.startColorComponent = startColorComponent
+        self.endColorComponent = endColorComponent
+        self.scene = scene
+        self.originPoint = originPoint
+        self.startingRadius = startingRadius
+        self.numPointsInWave = numPointsInWave
+        self.massOfPoint = massOfPoint
+        pointsOfCircle = []
+        circle = SKShapeNode()
+        
+        initCircle(scene : scene)
+        scene.addChild(circle)
+    }
+    
+    func initCircle(scene : SKScene) {
         let circlePath : CGMutablePath = CGMutablePath.init()
         
-        for angle in 0...360 {
+        for angle in 0...numPointsInWave {
             let physicsPoint = SKSpriteNode()
-            physicsPoint.position.x = cos(CGFloat(angle)*CGFloat.pi/180) * radiusOfUnaffectedCircle
-            physicsPoint.position.y = sin(CGFloat(angle)*CGFloat.pi/180) * radiusOfUnaffectedCircle
+            physicsPoint.position.x = cos(2*CGFloat(angle)*CGFloat.pi/CGFloat(numPointsInWave)) * startingRadius
+            physicsPoint.position.y = sin(2*CGFloat(angle)*CGFloat.pi/CGFloat(numPointsInWave)) * startingRadius
             physicsPoint.texture = SKTexture(image: #imageLiteral(resourceName: "hollowBall"))
-            //physicsPoint.physicsBody = SKPhysicsBody(circleOfRadius: 0.01)
-            //physicsPoint.physicsBody = SKPhysicsBody(circleOfRadius: 0.01)
             physicsPoint.physicsBody = SKPhysicsBody(texture: physicsPoint.texture!, size: CGSize(width: 0.09, height: 0.09))
             physicsPoint.physicsBody?.mass = massOfPoint
             physicsPoint.physicsBody?.pinned = false
@@ -141,9 +189,9 @@ class GameScene: SKScene {
             physicsPoint.physicsBody?.isDynamic = true
             physicsPoint.isHidden = false
             pointsOfCircle.append(physicsPoint)
-            addChild(physicsPoint)
+            scene.addChild(physicsPoint)
             physicsPoint.physicsBody!.applyForce(CGVector(dx: 3*physicsPoint.position.x, dy: 3*physicsPoint.position.y))
-            physicsPoint.position = CGPoint(x: physicsPoint.position.x - self.size.width / 2, y: physicsPoint.position.y - self.size.height / 2)
+            physicsPoint.position = CGPoint(x: physicsPoint.position.x + originPoint.x, y: physicsPoint.position.y + originPoint.y)
             if angle == 0 {
                 circlePath.move(to: physicsPoint.position)
             }
@@ -153,9 +201,8 @@ class GameScene: SKScene {
         }
         
         circle.path = circlePath
-        circle.strokeColor = UIColor.white
-        circle.lineWidth = 2
-        addChild(circle)
+        circle.strokeColor = UIColor(red : startColorComponent, green : startColorComponent, blue : startColorComponent, alpha : 1)
+        circle.lineWidth = 50
     }
     
     func updateCircle() {
@@ -163,26 +210,46 @@ class GameScene: SKScene {
         let circlePath : CGMutablePath = CGMutablePath.init()
         var nextIndex = 1
         circlePath.move(to: pointsOfCircle[0].position)
+        
         for point in pointsOfCircle {
             //point.physicsBody!.applyImpulse(CGVector(dx: 1, dy: 1))
-            point.physicsBody!.applyForce(vectorFromPoints(point1: point.position, point2: circlePath.currentPoint))
-            point.physicsBody!.applyForce(vectorFromPoints(point1: point.position, point2: pointsOfCircle[nextIndex].position))
+            point.physicsBody!.applyForce(GameScene.vectorFromPoints(point1: point.position, point2: circlePath.currentPoint))
+            point.physicsBody!.applyForce(GameScene.vectorFromPoints(point1: point.position, point2: pointsOfCircle[nextIndex].position))
             circlePath.addLine(to: point.position)
-            if nextIndex == 359 {
+            if nextIndex == numPointsInWave {
                 nextIndex = 0
             }
             else {
                 nextIndex += 1
             }
         }
+        
+        let elapsedTime = NSDate().timeIntervalSince1970 - birthTime
+        let timeLeft = lifeSpan - elapsedTime
+        
+        if (timeLeft <= 0) {
+            toBeDestroyedVar = true
+        }
+        
+        let colorComp = (startColorComponent - endColorComponent) * CGFloat((timeLeft) / (lifeSpan)) + endColorComponent
+        
+        circle.strokeColor = UIColor(red: colorComp, green: colorComp, blue: colorComp, alpha: CGFloat(timeLeft / lifeSpan))
+        
+        circlePath.addLine(to: pointsOfCircle[0].position)
         circle.path = circlePath
         circle.lineWidth = 2
-        addChild(circle)
+        scene.addChild(circle)
     }
     
-    func vectorFromPoints(point1 : CGPoint, point2 : CGPoint) -> CGVector {
-        let dx = point2.x - point1.x
-        let dy = point2.y - point1.y
-        return CGVector(dx: dx, dy: dy)
+    func toBeDestroyed() -> Bool {
+        return toBeDestroyedVar
+    }
+    
+    deinit {
+        circle.removeFromParent()
+        
+        for point in pointsOfCircle {
+            point.removeFromParent()
+        }
     }
 }
